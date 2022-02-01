b5c246b455323d69f80649a405b1462b11e9171b
karekauan <71836200+karekauan@users.noreply.github.com>
2022-02-01 12:32:44 -0300

    Código usando PyQt5 & sqlite3

--- Project/Login.py
+++ Project/Login.py
@@ -1,68 +1,86 @@
-# Importação de Bibliotecas #
-from genericpath import exists
-import PySimpleGUI as sg
-import sqlite3 
-#---------------------------#
+#imports#
+import sys
+import sqlite3
+import PyQt5
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import *
+#-------#
 
-# Criação do bd e seu cursor #
+#Criando o bd e seu cursor#
 bd = sqlite3.connect('Cadastros.db')
 cursor = bd.cursor()
-#----------------------------#
+#--------------------------#
 
-# Criação dos layouts no esquema linha/coluna e escolha do tema#
-sg.theme('Dark Gray 11')
+app = QtWidgets.QApplication(sys.argv) #Criando a variável de execução
+Screen_Log = uic.loadUi('Login.ui') #Atribuindo o respectivo arquivos da janela
+Screen_Cad = uic.loadUi('Cadastro.ui') #               |         |
+Screen_Logout = uic.loadUi('Logout.ui') #              |         |  
 
-Login_Layout = [
-    [sg.Text('Email:  '), sg.Input(key='Log', size=(30, 1))],
-    [sg.Text('Senha: '), sg.Input(key='Pass', size=(30, 1), password_char='*')],
-    [sg.Button('Login'), sg.Text('', size=(30, 1)), sg.Button('Cadastrar')]
-]
-
-Cadastro_Layout = [
-    [sg.Text('Informe seu email:   '), sg.Input(key='User', size=(30, 1))],
-    [sg.Text('Informe sua senha:  '), sg.Input(key='Key', size=(30, 1))],
-    [sg.Button('Cadastrar')],
-]
-#--------------------------------------------#
-
-Login_Tela = sg.Window('Tela de Login', Login_Layout) # Instanciando a janela de login
-Cadastro_Tela = sg.Window('Tela de Cadastro', Cadastro_Layout) # Instanciando a janela de cadastro
-
-def Login(): # Função para verificar campos vazios e conferir igualdade de credenciais
-    if Login_values['Log'] and Login_values['Pass'] != '':
-        email = Login_values['Log']
-        senha = Login_values['Pass']
+def Abre_Cadastro():
+    Screen_Cad.show()
+    
+def Fecha_Cadastro():
+    Screen_Cad.close()
+    
+def Cadastro(): #Função para cadastrar dados no bd
+    User = Screen_Cad.Input_Name.text() #Obtendo os dados do input
+    Email = Screen_Cad.Input_Email.text() #       |     |
+    Pass = Screen_Cad.Input_Pass.text() #         |     |
+    Pass_Confirm = Screen_Cad.Input_Pass_Confirm.text() #    |    |
+    
+    if (Pass == Pass_Confirm):
         try:
-            cursor.execute(f"SELECT senha FROM cadastros WHERE email = '{email}'")
-            senha_bd = cursor.fetchall()
-            if senha == senha_bd[0][0]:
-                sg.popup("LOGADO")
-            else:
-                sg.popup_error('Credencial Inválida, tente novamente')
-        except:
-            sg.popup_error('Credencial Inválida, tente novamente')
-    
-    
-def Cadastrar(): # Função para verificar campos vazios e cadastrar as credenciais nos arquivos
-    if Cadastro_event == 'Cadastrar':
-     cursor.execute("CREATE TABLE IF NOT EXISTS cadastros (email text, senha text)")   
-    if Cadastro_values['User'] and Cadastro_values['Key'] != '':
-        email = Cadastro_values['User']
-        senha = Cadastro_values['Key']
-        cursor.execute(f"INSERT INTO cadastros VALUES('{email}', '{senha}')")
-        bd.commit()
-        Cadastro_Tela.close()      
+            cursor.execute("CREATE TABLE IF NOT EXISTS cadastros(nome text, email text, senha text)")
+            cursor.execute("INSERT INTO cadastros VALUES('"+User+"', '"+Email+"', '"+Pass+"')")
+            
+            bd.commit()
+            Screen_Cad.Warning_Same_Pass.setText("Usuário Cadastrado")
+        except sqlite3.Error as erro:
+            print("Erro ao inserir os dados: ", erro)
     else:
-        sg.popup_error('Campo vazio, preencha e tente novamente')
+        Screen_Cad.Warning_Same_Pass.setText("Senhas não correpsondentes")
+        
+    Screen_Cad.Input_Name.setText("") #Resetando o valor dos inputs 
+    Screen_Cad.Input_Email.setText("") #          |    |
+    Screen_Cad.Input_Pass.setText("") #           |    |
+    Screen_Cad.Input_Pass_Confirm.setText("") #   |    |   
 
-while True: # Laço principal para repetição infinita
-    Login_event, Login_values = Login_Tela.read() # Obtenção dos valores e eventos da janela
-    if Login_event == sg.WINDOW_CLOSED or Login_event == 'Exit': # Encerramento do laço principal
-        break
-    if Login_event == 'Login':
-        Login()
-    if Login_event == 'Cadastrar':
-            Cadastro_event, Cadastro_values = Cadastro_Tela.read() # Obtenção dos valores e eventos da janela
-            if Cadastro_event == sg.WINDOW_CLOSED or Cadastro_event == 'Exit': # Encerramento da janela
-                Cadastro_Tela.close()
-            Cadastrar()
+def Login(): # Função para logar na conta
+    Email = Screen_Log.Input_User.text()
+    Key = Screen_Log.Input_Key.text()
+    try:
+        cursor.execute("SELECT senha FROM cadastros WHERE email = '"+Email+"'")
+        Key_bd = cursor.fetchall()
+        if (Key == Key_bd[0][0]):
+            Screen_Logout.show()
+            Screen_Log.close()
+                
+            cursor.execute("SELECT nome FROM cadastros WHERE email = '"+Email+"'")
+            User_Name = cursor.fetchall()
+            Screen_Logout.Ornament_Functional.setText(User_Name[0][0])
+            
+        else:
+            Screen_Log.Warning_Wrong_Credencial.setText("Senha Incorreta")
+    except:
+        Screen_Log.Warning_Wrong_Credencial.setText("Email Incorreto")
+        
+    Screen_Log.Input_User.setText("")
+    Screen_Log.Input_Key.setText("")
+        
+        
+
+def Logout(): #Função para deslogar da conta
+    Screen_Logout.close()
+    Screen_Log.show()
+    Screen_Log.Warning_Wrong_Credencial.setText("") #Zera o valor do aviso de erro
+    
+#Atribuindo funções aos Buttons#
+Screen_Log.Button_Log.clicked.connect(Login)
+Screen_Logout.Button_Out.clicked.connect(Logout)
+Screen_Log.Button_Reg.clicked.connect(Abre_Cadastro)
+Screen_Cad.Button_Back.clicked.connect(Fecha_Cadastro)
+Screen_Cad.Button_Cad.clicked.connect(Cadastro)
+#------------------------------#    
+
+Screen_Log.show() #Exeibindo a tela inicial
+sys.exit(app.exec_()) #Rodando a aplicação
